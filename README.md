bbc1c364fb2f44fce41fe54226add675c4d4d5fb
RaidonReyes <RaidonDaBoss@gmail.com>
2024-08-26 12:00:54 -0700

    Changes to hello.cpp, readme, and test.txt

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # CS120-FA24
 # CS120-FA24
+# My first github repository
--- hello.cpp
+++ hello.cpp
@@ -3,4 +3,7 @@ using namespace std;
 int main()
 {
         cout << "Hello World" << endl;
+        cout << "It is me" << endl;
+        cout << "Raidon Reyes" << endl;
+
 }
